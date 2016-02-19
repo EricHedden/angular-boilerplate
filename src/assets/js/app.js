abe77b28bc6d1921b4ac6dc033692dc45796cc94
Damiano Petrungaro <damianopetrungaro@gmail.com>
2016-02-19 13:17:29 +0100

    Fix module loading

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -14,6 +14,399 @@
 	angular.module('core.routing', ['ui.router']);
 
 })();
+(function(){
+
+	'use strict';
+
+	// Define angular app module
+	angular.module('app', ['core.routing', 'core.mocking', 'ui.router']);
+
+})();
+(function() {
+
+  'use strict';
+
+    // Pass the usersFactory to the app
+    angular
+        .module('app')
+        .factory('usersFactory', usersFactory);
+
+
+    // Inject dependecies in the usersFactory
+    usersFactory.$inject = ['$http'];
+
+    
+    // Define the usersFactory
+    function usersFactory($http) {
+
+
+        // Define base URI for user user
+        var userBase = '/api/v1/users/';
+
+
+        // Define the user factory object to return
+        var usersFactory = {
+
+            index: index,
+            show: show,
+            store: store,
+            update: update,
+            destroy: destroy,
+
+        };
+
+
+        // Return the user factory
+        return usersFactory;
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Functions
+        |--------------------------------------------------------------------------
+        |
+        | Declaring all functions used in the usersFactory
+        |
+        */
+       
+
+        // Display a listing of users.
+        function index() {
+
+            return $http.get(userBase)
+                        .then(function(data){return data});
+
+        };
+
+
+        // Display a specified user.
+        function show(id) {
+
+            return $http.get(userBase + id)
+                        .then(function(data){return data.data});
+
+        };
+
+
+        // Store a newly created user in storage.
+        function store(data) {
+
+            return $http.post(userBase, data)
+                        .then(function(data){return data.data});
+
+        };
+
+
+        // Update the specified user in storage.
+        function update(id, data) {
+
+            return $http.put(userBase + id, data)
+                        .then(function(data){return data.data});
+
+        };
+
+
+        // Remove the specified user from storage.
+        function destroy(id) {
+
+            return $http.delete(userBase + id)
+                        .then(function(data){return data.data});
+
+        };
+
+    }
+
+})();
+
+(function() {
+
+  'use strict';
+
+    // Pass the usersMock to the app
+	angular
+	    .module('app')
+	    .run(usersMock);
+
+
+    // Inject dependecies in the usersMock
+	usersMock.$inject = ['mockHelper'];
+
+
+	// Define the usersMock
+    function usersMock(mockHelper) {
+        
+
+        // Object for user's mock
+    	var users = {};
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Contrsucts function
+        |--------------------------------------------------------------------------
+        |
+        | All functions that should be init when the controller start
+        |
+        */
+
+    	
+    	setArrayUsers();																// Set the list of user
+    	mockHelper.configureMocks(getMocks()); 											// Intercept all the api and add them to the httpBackend
+
+
+
+	    /*
+	    |--------------------------------------------------------------------------
+	    | Functions
+	    |--------------------------------------------------------------------------
+	    |
+	    | Declaring all functions used in the usersMock
+	    |
+	    */
+
+
+		// Function for destroy users API
+		function destroyRespond(method, url, data, headers, params) {
+
+			//users.splice(0,1);
+			console.log(params);
+			// Get a random header
+			var header = randomHeader();
+
+			// If the result will be 200, execute the operation
+			if(header == 200) {
+
+				// Return the success header
+				return [header, {data: 'yes'}];
+			}
+
+			// Return the error header
+			return [header, {error:'error'}];
+		}
+
+
+		// Function for index users API
+		function indexRespond(method, url, data, headers, params) {
+
+			// Get a random header
+			var header = randomHeader();
+
+			// If the result will be 200, execute the operation
+			if(header == 200) {
+
+				// Return the success header
+				return [header, {data: users}];
+			}
+
+			// Return the error header
+			return [header, {error:'error'}];
+		}
+
+
+		// Function for show users API
+		function showRespond(method, url, data, headers, params) {
+
+			// Get a random header
+			var header = randomHeader();
+
+			// If the result will be 200, execute the operation
+			if(header == 200) {
+
+				// Get the data to return
+				var user = users[1];
+
+				// Return the success header
+				return [header, {data: user}];
+			}
+
+			// Return the error header
+			return [header, {error:'error'}];
+		}
+
+
+		// Function for store users API
+		function storeRespond(method, url, data, headers, params) {
+
+			// Get a random header
+			var header = randomHeader();
+
+			// If the result will be 200, execute the operation
+			if(header == 200) {
+
+				// Return the success header
+				return [header, {data: 'yes'}];
+			}
+
+			// Return the error header
+			return [header, {error:'error'}];
+		}
+
+
+		// Function for update users API
+		function updateRespond(method, url, data, headers, params) {
+
+			// Get a random header
+			var header = randomHeader();
+
+			// If the result will be 200, execute the operation
+			if(header == 200) {
+
+				// Return the success header
+				return [header, {data: 'yes'}];
+			}
+
+			// Return the error header
+			return [header, {error:'error'}];
+		}
+
+
+		// Basic algorithm for random headers 
+		function randomHeader(){
+
+			// Generate a random number from 1 to 10
+			var random = Math.floor((Math.random() * 10) + 1);
+
+			// Return 500 if random is 10
+			if(random == 10) {
+
+				return 500;
+			}
+
+			// Return 404 if random is 9
+			if(random == 9) {
+
+				return 404;
+			}
+
+			// Return 200
+			return 200;
+		}
+
+
+		// Function that pass the array that will create the httpBackend
+	    function getMocks() {
+
+	    	// Object to pass for fake API
+			return [{
+
+				label: 'destroy',
+			    method: 'DELETE',
+			    url: /\/api\/v1\/users\/(d*)/, //  Why '/api/v1/users/:id' not works here!?
+			    params: ['id'],
+			    respond: destroyRespond
+			
+			},{
+
+				label: 'index',
+			    method: 'GET',
+			    url: '/api/v1/users/',
+			    respond: indexRespond
+			
+			},{
+
+				label: 'show',
+			    method: 'GET',
+			    url: /\/api\/v1\/users\/(d*)/, //  Why '/api/v1/users/:id' not works here!?
+			    params: ['id'],
+			    respond: showRespond
+			
+			},{
+
+				label: 'store',
+			    method: 'POST',
+			    url: '/api/v1/users/',
+			    respond: storeRespond
+			
+			},{
+
+				label: 'update',
+			    method: 'PUT',
+			    url: /\/api\/v1\/users\/(d*)/, //  Why '/api/v1/users/:id' not works here!?
+			    params: ['id'],
+			    respond: updateRespond
+			}];
+		}
+
+
+		// Fucntion for set the array 
+		function setArrayUsers() {
+
+			return users = [{name: 'A'}, {name: 'B'}, {name: 'C'}];
+		}
+	}
+
+})();
+(function() {
+
+  'use strict';
+
+    // Pass the usersRoute to the app
+	angular
+	    .module('app')
+	    .run(usersRoute);
+
+
+    // Inject dependecies in the usersRoute
+	usersRoute.$inject = ['routerHelper'];
+
+
+	// Define the usersRoute
+    function usersRoute(routerHelper) {
+
+    	// Intercept all the states and add them to the routing
+    	routerHelper.configureStates(getStates());
+    }
+
+
+    // Define the getStates
+    function getStates() {
+
+		return [{
+			
+		    state: 'users-index',
+		    config: {
+		        url: '/users',
+		        templateUrl: 'App/Users/index/users.indexView.html',
+		        controller: 'usersIndexCtrl',
+		        controllerAs: 'usersIndex'
+		    }
+		}, {
+		    state: 'users-store',
+		    config: {
+		        url: '/users/store',
+		        templateUrl: 'App/Users/store/users.storeView.html',
+		        controller: 'usersStoreCtrl',
+		        controllerAs: 'usersStore'
+		    }
+		}, {
+		    state: 'users-show',
+		    config: {
+		        url: '/users/:id',
+		        templateUrl: 'App/Users/show/users.showView.html',
+		        controller: 'usersShowCtrl',
+		        controllerAs: 'usersShow'
+		    }
+		}, {
+		    state: 'users-update',
+		    config: {
+		        url: '/users/:id/update',
+		        templateUrl: 'App/Users/update/users.updateView.html',
+		        controller: 'usersUpdateCtrl',
+		        controllerAs: 'usersUpdate'
+		    }
+		}, {
+		    state: 'users-destroy',
+		    config: {
+		        url: '/users/:id/delete',
+		        templateUrl: 'App/Users/destroy/users.destroyView.html',
+		        controller: 'usersDestroyCtrl',
+		        controllerAs: 'usersDestroy'
+		    }
+		}]
+	}
+
+})();
+
 (function() {
 
   'use strict';
@@ -655,398 +1048,6 @@
 
 			}
 		}
-	}
-
-})();
-(function(){
-
-	'use strict';
-
-	// Define angular app module
-	angular.module('app', ['core.routing', 'core.mocking', 'ui.router']);
-
-})();
-(function() {
-
-  'use strict';
-
-    // Pass the usersFactory to the app
-    angular
-        .module('app')
-        .factory('usersFactory', usersFactory);
-
-
-    // Inject dependecies in the usersFactory
-    usersFactory.$inject = ['$http'];
-
-    
-    // Define the usersFactory
-    function usersFactory($http) {
-
-
-        // Define base URI for user user
-        var userBase = '/api/v1/users/';
-
-
-        // Define the user factory object to return
-        var usersFactory = {
-
-            index: index,
-            show: show,
-            store: store,
-            update: update,
-            destroy: destroy,
-
-        };
-
-
-        // Return the user factory
-        return usersFactory;
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | Functions
-        |--------------------------------------------------------------------------
-        |
-        | Declaring all functions used in the usersFactory
-        |
-        */
-       
-
-        // Display a listing of users.
-        function index() {
-
-            return $http.get(userBase)
-                        .then(function(data){return data});
-
-        };
-
-
-        // Display a specified user.
-        function show(id) {
-
-            return $http.get(userBase + id)
-                        .then(function(data){return data.data});
-
-        };
-
-
-        // Store a newly created user in storage.
-        function store(data) {
-
-            return $http.post(userBase, data)
-                        .then(function(data){return data.data});
-
-        };
-
-
-        // Update the specified user in storage.
-        function update(id, data) {
-
-            return $http.put(userBase + id, data)
-                        .then(function(data){return data.data});
-
-        };
-
-
-        // Remove the specified user from storage.
-        function destroy(id) {
-
-            return $http.delete(userBase + id)
-                        .then(function(data){return data.data});
-
-        };
-
-    }
-
-})();
-
-(function() {
-
-  'use strict';
-
-    // Pass the usersMock to the app
-	angular
-	    .module('app')
-	    .run(usersMock);
-
-
-    // Inject dependecies in the usersMock
-	usersMock.$inject = ['mockHelper'];
-
-
-	// Define the usersMock
-    function usersMock(mockHelper) {
-        
-
-        // Object for user's mock
-    	var users = {};
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | Contrsucts function
-        |--------------------------------------------------------------------------
-        |
-        | All functions that should be init when the controller start
-        |
-        */
-
-    	
-    	setArrayUsers();																// Set the list of user
-    	mockHelper.configureMocks(getMocks()); 											// Intercept all the api and add them to the httpBackend
-
-
-
-	    /*
-	    |--------------------------------------------------------------------------
-	    | Functions
-	    |--------------------------------------------------------------------------
-	    |
-	    | Declaring all functions used in the usersMock
-	    |
-	    */
-
-
-		// Function for destroy users API
-		function destroyRespond(method, url, data, headers, params) {
-
-			//users.splice(0,1);
-			console.log(params);
-			// Get a random header
-			var header = randomHeader();
-
-			// If the result will be 200, execute the operation
-			if(header == 200) {
-
-				// Return the success header
-				return [header, {data: 'yes'}];
-			}
-
-			// Return the error header
-			return [header, {error:'error'}];
-		}
-
-
-		// Function for index users API
-		function indexRespond(method, url, data, headers, params) {
-
-			// Get a random header
-			var header = randomHeader();
-
-			// If the result will be 200, execute the operation
-			if(header == 200) {
-
-				// Return the success header
-				return [header, {data: users}];
-			}
-
-			// Return the error header
-			return [header, {error:'error'}];
-		}
-
-
-		// Function for show users API
-		function showRespond(method, url, data, headers, params) {
-
-			// Get a random header
-			var header = randomHeader();
-
-			// If the result will be 200, execute the operation
-			if(header == 200) {
-
-				// Get the data to return
-				var user = users[1];
-
-				// Return the success header
-				return [header, {data: user}];
-			}
-
-			// Return the error header
-			return [header, {error:'error'}];
-		}
-
-
-		// Function for store users API
-		function storeRespond(method, url, data, headers, params) {
-
-			// Get a random header
-			var header = randomHeader();
-
-			// If the result will be 200, execute the operation
-			if(header == 200) {
-
-				// Return the success header
-				return [header, {data: 'yes'}];
-			}
-
-			// Return the error header
-			return [header, {error:'error'}];
-		}
-
-
-		// Function for update users API
-		function updateRespond(method, url, data, headers, params) {
-
-			// Get a random header
-			var header = randomHeader();
-
-			// If the result will be 200, execute the operation
-			if(header == 200) {
-
-				// Return the success header
-				return [header, {data: 'yes'}];
-			}
-
-			// Return the error header
-			return [header, {error:'error'}];
-		}
-
-
-		// Basic algorithm for random headers 
-		function randomHeader(){
-
-			// Generate a random number from 1 to 10
-			var random = Math.floor((Math.random() * 10) + 1);
-
-			// Return 500 if random is 10
-			if(random == 10) {
-
-				return 500;
-			}
-
-			// Return 404 if random is 9
-			if(random == 9) {
-
-				return 404;
-			}
-
-			// Return 200
-			return 200;
-		}
-
-
-		// Function that pass the array that will create the httpBackend
-	    function getMocks() {
-
-	    	// Object to pass for fake API
-			return [{
-
-				label: 'destroy',
-			    method: 'DELETE',
-			    url: /\/api\/v1\/users\/(d*)/, //  Why '/api/v1/users/:id' not works here!?
-			    params: ['id'],
-			    respond: destroyRespond
-			
-			},{
-
-				label: 'index',
-			    method: 'GET',
-			    url: '/api/v1/users/',
-			    respond: indexRespond
-			
-			},{
-
-				label: 'show',
-			    method: 'GET',
-			    url: /\/api\/v1\/users\/(d*)/, //  Why '/api/v1/users/:id' not works here!?
-			    params: ['id'],
-			    respond: showRespond
-			
-			},{
-
-				label: 'store',
-			    method: 'POST',
-			    url: '/api/v1/users/',
-			    respond: storeRespond
-			
-			},{
-
-				label: 'update',
-			    method: 'PUT',
-			    url: /\/api\/v1\/users\/(d*)/, //  Why '/api/v1/users/:id' not works here!?
-			    params: ['id'],
-			    respond: updateRespond
-			}];
-		}
-
-
-		// Fucntion for set the array 
-		function setArrayUsers() {
-
-			return users = [{name: 'A'}, {name: 'B'}, {name: 'C'}];
-		}
-	}
-
-})();
-(function() {
-
-  'use strict';
-
-    // Pass the usersRoute to the app
-	angular
-	    .module('app')
-	    .run(usersRoute);
-
-
-    // Inject dependecies in the usersRoute
-	usersRoute.$inject = ['routerHelper'];
-
-
-	// Define the usersRoute
-    function usersRoute(routerHelper) {
-
-    	// Intercept all the states and add them to the routing
-    	routerHelper.configureStates(getStates());
-    }
-
-
-    // Define the getStates
-    function getStates() {
-
-		return [{
-			
-		    state: 'users-index',
-		    config: {
-		        url: '/users',
-		        templateUrl: 'App/Users/index/users.indexView.html',
-		        controller: 'usersIndexCtrl',
-		        controllerAs: 'usersIndex'
-		    }
-		}, {
-		    state: 'users-store',
-		    config: {
-		        url: '/users/store',
-		        templateUrl: 'App/Users/store/users.storeView.html',
-		        controller: 'usersStoreCtrl',
-		        controllerAs: 'usersStore'
-		    }
-		}, {
-		    state: 'users-show',
-		    config: {
-		        url: '/users/:id',
-		        templateUrl: 'App/Users/show/users.showView.html',
-		        controller: 'usersShowCtrl',
-		        controllerAs: 'usersShow'
-		    }
-		}, {
-		    state: 'users-update',
-		    config: {
-		        url: '/users/:id/update',
-		        templateUrl: 'App/Users/update/users.updateView.html',
-		        controller: 'usersUpdateCtrl',
-		        controllerAs: 'usersUpdate'
-		    }
-		}, {
-		    state: 'users-destroy',
-		    config: {
-		        url: '/users/:id/delete',
-		        templateUrl: 'App/Users/destroy/users.destroyView.html',
-		        controller: 'usersDestroyCtrl',
-		        controllerAs: 'usersDestroy'
-		    }
-		}]
 	}
 
 })();
