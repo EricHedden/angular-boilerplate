36b32a25847340f4e5aa0c25a518e0fa723f2ee4
Damiano Petrungaro <damianopetrungaro@gmail.com>
2016-02-23 17:14:08 +0100

    minor fixes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -512,6 +512,62 @@
 
 })();
 
+(function() {
+
+  'use strict';
+
+    // Pass the staticsHomeCtrl to the app
+    angular
+        .module('app')
+        .controller('staticsHomeCtrl', staticsHomeCtrl);
+
+
+    // Define the staticsHomeCtrl
+    function staticsHomeCtrl() {
+
+
+        // Inject with ng-annotate
+        "ngInject";
+
+
+        // Define staticsHome as this for ControllerAs and auto-$scope
+        var staticsHome = this;
+            staticsHome.title =    "AngularJS-boilerplate";
+            staticsHome.content =  "A micro AngularJS boilerplate for start projects with no pain, based on JohnPapa style guide";
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Contrsucts function
+        |--------------------------------------------------------------------------
+        |
+        | All functions that should be init when the controller start
+        |
+        */
+
+
+        initLog();
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Functions
+        |--------------------------------------------------------------------------
+        |
+        | Declaring all functions used in the staticsHomeCtrl
+        |
+        */
+
+
+        // Sample for init function
+        function initLog() {
+
+            console.log('staticsHomeCtrl init');
+        }
+    }
+
+})();
+
 (function(){
 
 	'use strict';
@@ -665,62 +721,6 @@
 			}
 		}
 	}
-
-})();
-
-(function() {
-
-  'use strict';
-
-    // Pass the staticsHomeCtrl to the app
-    angular
-        .module('app')
-        .controller('staticsHomeCtrl', staticsHomeCtrl);
-
-
-    // Define the staticsHomeCtrl
-    function staticsHomeCtrl() {
-
-
-        // Inject with ng-annotate
-        "ngInject";
-
-
-        // Define staticsHome as this for ControllerAs and auto-$scope
-        var staticsHome = this;
-            staticsHome.title =    "AngularJS-boilerplate";
-            staticsHome.content =  "A micro AngularJS boilerplate for start projects with no pain, based on JohnPapa style guide";
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | Contrsucts function
-        |--------------------------------------------------------------------------
-        |
-        | All functions that should be init when the controller start
-        |
-        */
-
-
-        initLog();
-
-
-        /*
-        |--------------------------------------------------------------------------
-        | Functions
-        |--------------------------------------------------------------------------
-        |
-        | Declaring all functions used in the staticsHomeCtrl
-        |
-        */
-
-
-        // Sample for init function
-        function initLog() {
-
-            console.log('staticsHomeCtrl init');
-        }
-    }
 
 })();
 
